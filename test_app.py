1fc839b8bd8ada030add0f40858b1a12e6f0bf8d
Yash Randhe <yash.randhe164@gmail.com>
2026-04-03 11:28:51 -0400

    added bug in code

--- test_app.py
+++ test_app.py
@@ -15,10 +15,10 @@ def test_subtract():
 def test_multiply():
     assert multiply(3, 4 == 12)
 
-def test_divide(:
+def test_divide():
     assert divide(10, 2) == 5.0
 
-def test_divide_by_zero():
+def test_divide_by_zero(:
     with pytest.raises(ValueError):
         divide(10, 0)
 
