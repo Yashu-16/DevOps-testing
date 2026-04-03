97218eea80152693f3ff9f3a99ce308efaf64b72
Yash Randhe <yash.randhe164@gmail.com>
2026-04-03 11:04:27 -0400

    commit all

--- test_app.py
+++ test_app.py
@@ -12,8 +12,8 @@ def test_add():
 def test_subtract():
     assert subtract(10, 4) == 6
 
-def test_multiply(:
-    assert multiply(3, 4) == 12
+def test_multiply():
+    assert multiply(3, 4 == 12
 
 def test_divide():
     assert divide(10, 2) == 5.0
