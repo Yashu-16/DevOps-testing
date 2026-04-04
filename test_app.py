9c1b7efbf9b27d94d453228fe4f4f528966e28f0
Yash Randhe <yash.randhe164@gmail.com>
2026-04-03 20:15:33 -0400

    broken code

--- test_app.py
+++ test_app.py
@@ -12,13 +12,13 @@ def test_add():
 def test_subtract():
     assert subtract(10, 4) == 6
 
-def test_multiply():
+def test_multiply(:
     assert multiply(3, 4 == 12)
 
 def test_divide():
     assert divide(10, 2) == 5.0
 
-def test_divide_by_zero(:
+def test_divide_by_zero():
     with pytest.raises(ValueError):
         divide(10, 0)
 
